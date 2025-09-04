ed4a36525432a31080f973912e559d6d2553c71a
JuanDzk <juandiegocalvoosorio@gmail.com>
2025-09-04 12:20:18 -0500

    service role

--- services/roleService.js
+++ services/roleService.js
@@ -10,4 +10,59 @@ const getAllRoles = async () =>{
     }
 };
 
-const getUser =async
+const getRole = async(id) => {
+    try {
+        let roles = await db.Role.findAll();
+        return roles;
+    } catch (error) {
+        return error.message || "Failed to get roles";
+    }
+};
+
+const createRol = async (name,email,password) => {
+    try {
+        let newRole = await db.Rol.create({
+            name,
+            email,
+            password,
+        });
+    } catch (error) {
+        return error.message || "Rol could not be created";
+    }
+};
+const updateRol = async (id,name,email,password) => {
+    try {
+        let updatedRol = await db.Rol.update({
+            name,
+            email,
+            password
+        }, {
+            where : {
+                id,
+            }
+        });
+        return updatedRol;
+    } catch (error) {
+        return error.message || "Rol could not be updated";
+    }
+};
+
+const deleteRol = async (id) => {
+    try {
+        const deletedRole = await db.Rol.destroy({
+            where: {
+                id,
+            }
+        });
+        return deletedRole
+    } catch (error) {
+        return error.message || "Rol could not be deleted";
+    }
+}
+module.exports = {
+    getAllRoles,
+    getRole,
+    createRol,
+    updateRol,
+    deleteRol,
+};
