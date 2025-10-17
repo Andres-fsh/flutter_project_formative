8c1286e65a07777181485451be18068bce2bf818
Andres F <andresfsh7@gmail.com>
2025-10-16 19:17:38 -0500

    update phone2

--- models/applicationforms.js
+++ models/applicationforms.js
@@ -14,7 +14,7 @@ module.exports = (sequelize, DataTypes) => {
     name: DataTypes.STRING,
     identificationType: DataTypes.STRING,
     email: DataTypes.STRING,
-    phone: DataTypes.STRING,
+    phone: DataTypes.STRING(15),
     companyName: DataTypes.STRING,
     description: DataTypes.TEXT,
     fkIdTypeForms: DataTypes.INTEGER
